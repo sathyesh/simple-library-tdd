31929fc93e8b353d999552d11f8e7bea6ce92387
Sathyesh <sathyesh.fns@gmail.com>
2020-08-23 14:39:44 +0200

    feat: added server and express app initial code

--- bin/www.js
+++ bin/www.js
@@ -3,83 +3,60 @@
 /**
  * Module dependencies.
  */
-
-var app = require("../src/app");
-var http = require("http");
+require('dotenv/config');
+const http = require('http');
+const app = require('../src/app');
 
 /**
  * Get port from environment and store in Express.
  */
-
-var port = normalizePort("3000");
-app.set("port", port);
+const port = 3000;
+app.set('port', process.env.PORT || port);
 
 /**
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-
-  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
+const server = http.createServer(app);
 
 /**
  * Event listener for HTTP server "listening" event.
  */
 
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+const onListening = () => {
   console.log(`library management server listening on port ${port}`);
-}
+};
+
+/**
+* Event listener for HTTP server "error" event.
+*/
+
+const onError = (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+/**
+* Listen on provided port, on all network interfaces.
+*/
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
---
+++ src/app.js
@@ -0,0 +1,56 @@
+/**
+ * Express server is initialized
+ * MongoDB connection is established
+ * REST APIs are added to the server
+ */
+
+require('dotenv/config');
+const express = require('express');
+const mongoose = require('mongoose');
+const createError = require('http-errors');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+mongoose.connect('mongodb://localhost/LibraryDB', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+}).then(() => console.log('Connected to MongoDB at mongodb://localhost/LibraryDB...'))
+  .catch((err) => {
+    console.log('Failed to connect to MongoDB...', err);
+    process.exit();
+  });
+
+app.get('/', (req, res) => {
+  res.send('Library REST API Server is running');
+});
+
+// REST API for Users and Books
+const usersRouter = require('./routes/users.route');
+const booksRouter = require('./routes/books.route');
+const { USERS_API, BOOKS_API } = require('./models/constants');
+
+app.use(USERS_API, usersRouter);
+app.use(BOOKS_API, booksRouter);
+
+// Common 404 error for API Request
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  // render the error page
+  res.status(err.status || 500);
+  res.send(err);
+});
+
+module.exports = app;
