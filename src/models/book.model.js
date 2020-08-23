de3ca7a282a18844bfacc37520c49c3c6b85d362
Sathyesh <sathyesh.fns@gmail.com>
2020-08-23 14:40:49 +0200

    code: added models for books and users

---
+++ src/models/book.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  isbn: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 255,
+  },
+  title: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 255,
+  },
+  available_status: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  available_copies: {
+    type: Number,
+    required: false,
+    default: 1,
+  },
+});
+
+module.exports.Book = mongoose.model('book', bookSchema);
---
+++ src/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  books_issued: {
+    type: Array,
+    required: false,
+    default: [],
+  },
+});
+
+module.exports.User = mongoose.model('user', userSchema);
